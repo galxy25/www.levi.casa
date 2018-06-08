725e43a8a3bd6f0c45ac114ed621fdbfca23eedb
Levi Schoen <levismschoen@gmail.com>
2018-06-08 15:16:33 -0700

    Visual implementation of home page design with place holder values.

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Init
+// Load Foundation
+$(document).foundation();
+// Insure user action map hot areas are in the right place
+imageMapResize();
+// Update placeholder elements for date and time
+// with current local values
+let current_timestamp = new Date()
+// Plus one for the month because it's the only zero
+// based calendar attribute :facepalm:
+// https://stackoverflow.com/questions/1507619/javascript-date-utc-function-is-off-by-a-month
+let current_date = current_timestamp.getFullYear() + '/' +(current_timestamp.getMonth() + 1) + '/' + current_timestamp.getDate()
+let current_time =current_timestamp.getHours() + ':' + current_timestamp.getMinutes() + ':' + current_timestamp.getSeconds()
+$("#current-date > a").text(current_date)
+$("#current-time > a").text(current_time)
+// React
